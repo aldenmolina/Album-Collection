b4621dbc4d42cf0abfa38fa732c6ec5c86ff1d2f
Alden <aj.molina092@gmail.com>
2019-03-08 12:00:14 -0500

    Added album model props and display

--- AlbumCollection/Models/Album.cs
+++ AlbumCollection/Models/Album.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace AlbumCollection.Models
 {
     public class Album
     {
+        [Key]
+        public int AlbumId { get; set; }
+        
+        [Display(Name = "Album Title")]
+        public string Name { get; set; }
+
+        [Display(Name = "Artist Name")]
+        public string ArtistName { get; set; }
+
     }
 }

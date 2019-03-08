dee5cd64476fd341186d5a97d7b97296a24b8c22
Alden <aj.molina092@gmail.com>
2019-03-08 15:42:05 -0500

    Added album index and details tests

--- AlbumCollection.Tests/AlbumControllerTests.cs
+++ AlbumCollection.Tests/AlbumControllerTests.cs
@@ -1,3 +1,7 @@
+using AlbumCollection.Controllers;
+using AlbumCollection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
 using System;
 using Xunit;
 
@@ -5,10 +9,29 @@ namespace AlbumCollection.Tests
 {
     public class AlbumControllerTests
     {
-        [Fact]
-        public void Index_Sets_Model_To_All_Albums()
-        {
+        AlbumController underTest;
+        private IAlbumRepository repo;
 
+        public AlbumControllerTests()
+        {
+            repo = Substitute.For<IAlbumRepository>();
+            underTest = new AlbumController(repo);
+        }
+
+        [Fact]
+        public void Index_Returns_A_View_Result()
+        {
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+        public void Details_Has_A_View()
+        {
+            var result = underTest.Details(1);
+
+            Assert.IsType<ViewResult>(result);
         }
     }
 }

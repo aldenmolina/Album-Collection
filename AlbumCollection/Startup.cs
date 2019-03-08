4aaa2105df1ce6f0caa1f22ca82e94d033620432
Alden <aj.molina092@gmail.com>
2019-03-08 12:43:52 -0500

    Added a home view page

--- AlbumCollection/Startup.cs
+++ AlbumCollection/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AlbumCollection.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -17,7 +18,8 @@ namespace AlbumCollection
         {
             services.AddMvc();
             services.AddDbContext<AlbumContext>();
-
+            services.AddScoped<IAlbumRepository, AlbumRepository>();
+            services.AddScoped<ISongRepository, SongRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

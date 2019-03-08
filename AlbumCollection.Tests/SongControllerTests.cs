b777215a578ecdad50fac45f5325b27e3540b744
Alden <aj.molina092@gmail.com>
2019-03-08 17:57:24 -0500

    Fixed song repo and controller with tests

---
+++ AlbumCollection.Tests/SongControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using AlbumCollection.Controllers;
+using AlbumCollection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AlbumCollection.Tests
+{
+    public class SongControllerTests
+    {
+        [Fact]
+        public void Can_Create_Song()
+        {
+            var songRepo = new SongRepository(null);
+            var underTest = new SongController(songRepo);
+
+            var result = underTest.Create();
+
+            Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
---
+++ AlbumCollection/Controllers/SongController.cs
@@ -0,0 +1,32 @@
+﻿using AlbumCollection.Models;
+using AlbumCollection.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlbumCollection.Controllers
+{
+    public class SongController : Controller
+    {
+        SongRepository songRepo;
+        public SongController(SongRepository songRepo)
+        {
+            this.songRepo = songRepo;
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Song song)
+        {
+            songRepo.Create(song);
+            return RedirectToAction("Index");
+        }
+    }
+}

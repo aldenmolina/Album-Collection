c038945815239c8ded681ef649c84dee6e4bf529
Alden <aj.molina092@gmail.com>
2019-03-08 12:01:58 -0500

    Added song model props and display

--- AlbumCollection/Models/Song.cs
+++ AlbumCollection/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace AlbumCollection.Models
 {
     public class Song
     {
+        [Key]
+        public int SongId { get; set; }
+
+        [Display(Name = "Song Name")]
+        public string Name { get; set; }
     }
 }

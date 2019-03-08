052c390e1dcb95c4fee72cf5bed9a0062eeadcdb
Alden <aj.molina092@gmail.com>
2019-03-08 12:20:13 -0500

    Added song repository

--- AlbumCollection/Repositories/SongRepository.cs
+++ AlbumCollection/Repositories/SongRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AlbumCollection.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,21 @@ namespace AlbumCollection.Repositories
 {
     public class SongRepository
     {
+        AlbumContext db;
+        public SongRepository(AlbumContext db)
+        {
+            this.db = db;
+        }
+
+
+        public Song GetByID(int id)
+        {
+            return db.Songs.Single(song => song.SongId == id);
+        }
+
+        public IEnumerable<Song> GetAll()
+        {
+            return db.Songs.ToList();
+        }
     }
 }
